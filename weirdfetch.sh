f7a227ecb8e2b4f56f2ef6d2f0cc6889aef3b231
Synergic <38535438+synergic0084@users.noreply.github.com>
2020-11-16 18:18:42 +0100

    Minor changes to the code
    Added some comments, made the DE section a variable, and made it change to KDE Plasma if you're using KDE.

--- weirdfetch.sh
+++ weirdfetch.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
- 
+
+#Variables
+STR="$XDG_CURRENT_DESKTOP"
+
 echo Distro: "$(lsb_release -is)"
 echo Kernel: "$(uname -r)"
 echo Installed packages: "$(dpkg -l | grep -c '^ii')"
 echo PC Uptime: "$(uptime)"
 echo ===========================
-echo Desktop Enviroment: $XDG_CURRENT_DESKTOP
+if [ $XDG_CURRENT_DESKTOP=KDE ] #Checks if the user is running KDE
+then
+STR="KDE Plasma" #If the user is running KDE, change $STR to KDE Plasma
+fi
+echo Desktop Enviroment:  $STR
+
 echo CPU: "$(lscpu | sed -nr '/Model name/ s/.*:\s*(.*) @ .*/\1/p')" 
 echo "CPU Usage: `LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}'`% RAM Usage: `free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }'` HDD Usage: `df -h / | awk '/\// {print $(NF-1)}'`"
